51ffabab978e019e562e5545ec8fd3952eb0d47f
“shorooq” <shorooqalbahri2@gmail.com>
2020-04-30 02:28:54 +0400

    sample massage

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,8 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
          print("welcome to IOS Tutorial");
-         print("this is GituseExample");
+         
         
+        print("this is GitUseExample");
     }
 
 
